e61bd840433d3a0cbe7b7ecbae9f7e06d02bd8dd
pabllofelipe <pabllofelipe@hotmail.com>
2021-10-12 23:10:11 -0300

    Constraits and validation tests created

--- grails-app/domain/hubbub/User.groovy
+++ grails-app/domain/hubbub/User.groovy
@@ -6,5 +6,8 @@ class User {
     String homepage
     Date dateCreated
     static constraints = {
+        loginId size: 3..20, unique: true, nullable: false
+        password size: 6..8, nullable: false
+        homepage url: true, nullable: true
     }
 }
--- src/integration-test/groovy/hubbub/UserIntegrationSpec.groovy
+++ src/integration-test/groovy/hubbub/UserIntegrationSpec.groovy
@@ -18,8 +18,10 @@ class UserIntegrationSpec extends Specification {
         given: "A brand new user"
         def joe = new User(loginId: 'joe', password: 'secret',
                 homepage: 'http://www.grailsinaction.com')
+
         when: "the user is saved"
         joe.save()
+
         then: "it saved successfully and can be found in the database"
         joe.errors.errorCount == 0
         joe.id != null
@@ -31,10 +33,12 @@ class UserIntegrationSpec extends Specification {
         def existingUser = new User(loginId: 'joe', password: 'secret',
                 homepage: 'http://www.grailsinaction.com')
         existingUser.save(failOnError: true)
+
         when: "A property is changed"
         def FoundUser = User.get(existingUser.id)
         FoundUser.password = "changed"
         FoundUser.save(failOnError: true)
+
         then: "the change is reflected to database"
         User.get(existingUser.id).password == 'changed'
     }
@@ -44,10 +48,43 @@ class UserIntegrationSpec extends Specification {
         def existingUser = new User(loginId: 'joe', password: 'secret',
                 homepage: 'http://www.grailsinaction.com')
         existingUser.save(failOnError:true)
+
         when: "The user is deleted"
         def FoundUser = User.get(existingUser.id)
         FoundUser.delete(flush: true)
+
         then: "the user was deleted from database"
         !User.exists(FoundUser.id)
+    }
+
+    def "Saving an user with invalid properties causes an error"(){
+        given: "An new user"
+        def user = new User(loginId: "joe", password: 'tiny',
+                            homepage: 'not-a-url')
+
+        when: "the user is validated"
+        user.validate()
+
+        then:
+        user.hasErrors()
+        !user.errors.getFieldError("loginId")
+    }
+
+    def "recovering from a failing save in database due invalid properties"(){
+        given: "An new user that has invalid properties"
+        def user = new User(loginId: "joe", password: 'tiny',
+                homepage: 'not-a-url')
+        assert user.save() == null
+        assert user.hasErrors()
+
+        when: "We fix invalid properties"
+        user.password = "tiny123"
+        user.homepage = "http://www.grailsinaction.com"
+        user.validate()
+
+        then: "the user saves and validate is fine"
+        !user.hasErrors()
+        user.save()
+
     }
 }
